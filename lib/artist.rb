c7be30b4272c7ebc8049ca196cf4cb29915b2239
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-07 17:31:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
  extend Memorable::ClassMethods
  extend Findable::ClassMethods
  include Paramable::InstanceMethods
- include Memorable::
+ include Memorable::InstanceMethods
   @@artists = []
 
   def initialize
