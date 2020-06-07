1dd7cf57dfa3c3f23a62d63ea591dbc0bf8e4c3b
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-07 17:01:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
  extend Memorable::ClassMethods
- 
+ include Paramable::InstanceMethods
 # include Memorable::InstanceMethods
   @@songs = []
 
