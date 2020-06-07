3bb146d08984f037317ff0d02e21f83ebc9b940d
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-07 17:08:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,8 @@ class Song
   attr_accessor :name
   attr_reader :artist
  extend Memorable::ClassMethods
+ extend Findable::ClassMethods
  include Paramable::InstanceMethods
- include Memorable::InstanceMethods
   @@songs = []
 
   def initialize
